666ffbd341c246bb5a3ce31423018dab3000c972
oskadoskaposka <alepaduanlima@gmail.com>
2021-07-25 03:22:18 -0300

    teste maluco

--- get_next_line.c
+++ get_next_line.c
@@ -6,48 +6,140 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 20:51:55 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/07/24 22:40:01 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/07/25 03:21:47 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
+int	ft_strlen(const char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+		i++;
+	return (i);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	char	*dest;
+	size_t	size;
+
+	i = 0;
+	dest = malloc ((len + 1) * sizeof(char));
+	if (!dest || !s)
+		return (NULL);
+	size = ft_strlen(s);
+	while (((start + i) < size) && (s[start + i] != '\0') && (i < len))
+	{
+		dest[i] = s[start + i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+char	*ft_strchr(const char *s, int c)
+{
+	while (*s != (unsigned char)c)
+	{
+		if (*s == '\0')
+			return (NULL);
+		s++;
+	}
+	return ((char *)s);
+}
+
+char	*ft_strdup(const char *str)
+{
+	int		len;
+	char	*dest;
+
+	len = ft_strlen(str) + 1;
+	dest = malloc(len);
+	if (!dest)
+		return (NULL);
+	ft_memcpy(dest, str, len);
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char			*dest;
+	unsigned int	i;
+	unsigned int	j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	dest = malloc ((ft_strlen(s1) + ft_strlen(s2) + 1));
+	if (!dest)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	while (s2[j] != '\0')
+	{
+		dest[i + j] = s2[j];
+		j++;
+	}
+	dest[i + j] = '\0';
+	return (dest);
+}
+
+
+
 char	*get_next_line(int fd)
 {
-	char		*print;
+	char		*print[2]; // 0 é retorno e 1 sendo store temporario
 	static char	*temp;
 	static int	pass;
 	char		*buffer[BUFFER_SIZE + 1];
 	int			size;
 
-	
 	size = 1;
 	while (size > 0)
 	{
 		if (temp > 0)
 		{
-			//procura '\n'
-				//achou
-					//print = print + começo da temp ate o '\n'
-					//limpa temp
-					//temp = substr (depois do '/n' até o final da temp)
-					//pass += pass
-					//return print
-				//n achou
-					//print = strjoin(print + temp)
+			if (ft_strchr(temp, '\n')) //procura '\n' na temp
+			{//achou
+				print[0] = ft_substr(temp, 0, (ft_strchr(temp, '\n') - temp + 1)); //print[0] = começo da temp ate o '\n'
+				print[1] = ft_substr(temp, (ft_strchr(temp, '\n') - temp + 1), (ft_strlen(temp) - (ft_strchr(temp, '\n') - temp))); //print[1] = substr (depois do '/n' até o final da temp)
+				free (temp); //limpa temp
+				temp = ft_strdup(print[1]); //temp = substr (depois do '/n' até o final da temp)
+				free(print[1]);
+				pass = pass + 1; //pass += pass
+				return (print[0]); //return (print)
+			}
+			else
+			{//n achou
+				print[0] = ft_strjoin(print[0], temp); //print = strjoin(print + temp)
+			}
 		}
 		size = read (fd, buffer, BUFFER_SIZE);
 		buffer[size] = '\0';
 		if (!temp && size > 0)
-			//temp = strdup(buffer)
-			//volta la pra cima
+			temp = ft_strdup(buffer); //temp = strdup(buffer)
 		else if (temp && size > 0)
-			//temp = strjoin(temp + buffer)
+			print[1] = ft_strdup(temp);
+			free (temp);
+			temp = ft_strjoin(temp, buffer); //temp = strjoin(temp + buffer)
+			free (print[1]);
 		if (size == 0 && print > 0 && pass > 0)
-			//print = strjoin (print, "\n")
-
-
+		{
+			print = strjoin (print, "\n") //print = strjoin (print, "\n")
+			free (temp); //limpa temp
+			temp = NULL; //temp = NULL
+			return (print); //return (print)
+		}
 	}
-	
+	return (NULL);
 	
 }	
--- get_next_line.h
+++ get_next_line.h
@@ -6,15 +6,22 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 20:51:51 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/07/23 21:59:32 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/07/25 03:13:23 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE
+#ifndef GET_NEXT_LINE
 # define GET_NEXT_LINE
 
-# ifndef BUFFER_SIZE
+#ifndef BUFFER_SIZE
 # define BUFFER_SIZE 42
-# endif
+#endif
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <fcntl.h> //retirar, serve para a função open apenas
+
+char	*get_next_line(int fd);
+
 
 #endif
