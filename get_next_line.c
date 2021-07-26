f8b644c8b6a8a4ef3550b166e0bbec28d1978b9a
oskadoskaposka <alepaduanlima@gmail.com>
2021-07-26 01:25:10 -0300

    banana

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: apaduan- <apaduan-@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 20:51:55 by apaduan-          #+#    #+#             */
-/*   Updated: 2021/07/26 01:18:37 by apaduan-         ###   ########.fr       */
+/*   Updated: 2021/07/26 01:25:05 by apaduan-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -144,6 +144,7 @@ char	*get_next_line(int fd)
 			}
 			else
 			{
+				free(print[0]);
 				print[0] = ft_strdup(temp); 
 			}
 		}
